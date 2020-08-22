44da04fa0823295215d6fcedfb4201acf65b5a38
emiko takahashi <emiko_hashiba@yahoo.co.jp>
2020-08-22 13:06:10 +0000

    Finish full-fledged-login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user # 保存成功後、ログインします。
       flash[:success] = '新規作成に成功しました。'
-      redirect_to @user
+      redirect_to @user 
     else
       render :new
     end
